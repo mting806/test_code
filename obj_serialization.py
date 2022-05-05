4bccf2229f62157dbf0c5dbc62fa40bbad316bb3
mting <mting806@msn.com>
2022-05-05 08:49:37 +0200

    up

--- obj_serialization.py
+++ obj_serialization.py
@@ -22,14 +22,14 @@ print(a.get_data())
 
 with open("pickle_test.pickle", "wb") as f:
     pickle.dump(a, f, pickle.HIGHEST_PROTOCOL)
-with shelve.open("shelf_test.shelf", "c") as shelf:
+with shelve.open("shelf_test.shelf", "c", pickle.HIGHEST_PROTOCOL) as shelf:
     shelf["a"] = a
 del a
 
 b = pickle.load(open("pickle_test.pickle", "rb", pickle.HIGHEST_PROTOCOL))
 print(b.get_data())
 
-with shelve.open("shelf_test.shelf", "r") as shelf:
+with shelve.open("shelf_test.shelf", "r", pickle.HIGHEST_PROTOCOL) as shelf:
     for key in shelf.keys():
         print(repr(key), repr(shelf[key]))
     c = shelf["a"]
